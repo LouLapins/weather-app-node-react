b8cb02784c648df83c3324f73b771dbb7909d9f7
LouLapins <loulapins@hotmail.com>
2022-07-08 16:31:31 +0200

    Add router

---
+++ server/router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const fetch = require('node-fetch');
+
+let router = express.Router();
+
+router.get('/all', (req, res) => {
+
+    const url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json";
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            res.send({ data });
+        })
+        .catch(err => {
+            res.redirect('/error');
+        });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,10 +4,15 @@ require('dotenv').config();
 
 const PORT = process.env.PORT || 8000;
 
+const router = require('./router');
+
+app.use("/weather/", router);
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.send('Hello');
 })
-
 
 app.listen(PORT, (err) => {
     if (err) { console.log(err) };
