f70fff5e21e6b600c17fe70adfb0f540d7a40ec8
LouLapins <loulapins@hotmail.com>
2022-07-11 17:27:16 +0200

    Get date clicked from route parameter

--- client/src/components/Day.tsx
+++ client/src/components/Day.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function Day() {
+
+  const { date } = useParams();
+
   return (
     <div>
-        Hello
+        {date}
     </div>
   )
 }
