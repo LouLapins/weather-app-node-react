3999585a13e586c3fce3999042852f4745dd306e
LouLapins <loulapins@hotmail.com>
2022-07-15 22:03:23 +0200

    Resize image

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
-    <div className="max-w-[100vw] overflow-hidden min-h-screen bg-cover bg-palace min-w-screen App">
+    <div className="max-w-[100vw] overflow-hidden min-h-screen bg-palace-small bg-cover md:bg-palace min-w-screen App">
       <Routes>
           <Route path='/' element={<Landing/>} />
           <Route path="*" element={<PageNotFound />} />
